dc7cb4e59b5fe0499a91b3d50d44a4f737e24e5d
Latyas <latyas@gmail.com>
2014-04-25 11:30:28 +0800

    updated

--- 编译原理/LL1.py
+++ 编译原理/LL1.py
@@ -23,35 +23,130 @@ class LL1Parser:
         """
         if attr != 'T' and attr != 'NT':
             raise TypeError("符号属性设置错误")
-    
+    def add_derivation(self):
+        pass
+    def set_start_symbol(self, symbol):
+        self.start_symbol = symbol
+
     def cal_first(self,symbol):
+        #遇到终止符直接返回
+        if symbol.attr == 'T':
+            return set([symbol])
+
+        try:
+            if self.FIRST[symbol] != set():
+                return self.FIRST[symbol]
+        except:
+            self.FIRST[symbol] = set()
         #symbol.is_terminal 则直接返回只有该符号的集合
         #选出STATEMENTS中key=symbol的推导式
+        sts = self.STATEMENTS[symbol]
+        for st in sts:
         #以此遍历右端每个符号
-            #如果当前遍历到的符号是symbol，则函数返回不做处理
-        """
-        依据：
-            1. A->Ac
-            2. A->Bd
-            3. B->r
-            如果没有2.则1.会进入无限递归
-            考虑有推导A->Ac->Bdc->rdc, r是A的First集中的元素
-            忽略掉左递归的1,通过2,3也一样可以得出同样的结果
-            需要提交的语法没有不可化简的左递归
-        """
+            eps = True
+            for sym in st:
+            #如果当前遍历到的符号是symbol，且该符号不是终止符，则函数返回不做处理
+                if sym == symbol:
+                    return
+
+        
+        #依据：
+        #    1. A->Ac
+        #    2. A->Bd
+        #    3. B->r
+        #    如果没有2.则1.会进入无限递归
+        #    考虑有推导A->Ac->Bdc->rdc, r是A的First集中的元素
+        #    忽略掉左递归的1,通过2,3也一样可以得出同样的结果
+        #    需要提交的语法没有不可化简的左递归
+        #
             #获得该符号的first集
+                first = self.cal_first(sym)
             #如果该符号的first集不包含ESP，则symbol的first集为这个first集
+                if self.EPSILON not in first:
+                    self.FIRST[symbol].update(first)
+                    eps = False
+                    break
             #如果该符号的first集包含ESP，则symbol的first集并上这个first集后，继续扫描下一个符号
+                else:
+                    self.FIRST[symbol].update(first)
             #若所有的符号都可以导出ESP，则将ESP加入symbol的first集
-        pass
-    
-    
+            if eps: self.FIRST[symbol].add(self.EPSILON)
+        return self.FIRST[symbol] 
+
     def cal_follow(self,symbol):
         #symbol.is_terminal 则直接返回只有该符号的集合
+        if symbol.attr == 'T':
+            return set([symbol])
+        try:
+            if self.FOLLOW[symbol] != set():
+                return self.FOLLOW[symbol]
+        except:
+            self.FOLLOW[symbol] = set()
+
         #遍历STATEMENTS中右端含有symbol的推导式
+        
+        sts = []
+        for k,v in self.STATEMENTS.items():
+            for s in v:
+                if symbol in s: sts.append((k,s))
             #A->aBcd，symbol=B
             #1. A->aB，即B后无符号： FOLLOW(B)内加入FOLLOW(A)
+        for key,st in sts:
+            searched = st[st.index(symbol):]
+            if len(searched) == 1: 
+                follow = self.cal_follow(key)
+                self.FOLLOW[symbol].update(follow)
+                continue
+            
+            esp = True
+
             #2. A->aBc，c的first集为terminal，无ESP , FOLLOW(B)内加入FIRST(c)
+            for sym in searched[1:]:
+                first = self.cal_first(sym)
+                if self.EPSILON not in first:
+                    self.FOLLOW[symbol].update(first)
+                    esp = False
+                    break
             #3. A->aBcd, c的first集中有ESP，则FOLLOW(A)加入去掉ESP的FIRST(c),并扫描下一个符号
+                else:
+                    foo = first
+                    foo.remove(self.EPSILON)
+                    self.FOLLOW[symbol].update(foo)
             #如果B后的符号都可以导出ESP,则运用规则1后在FOLLOW(B)中加入ESP
-        pass
+            if esp: self.FOLLOW[symbol].update(set([self.EPSILON]))
+        try:
+            if symbol == self.start_symbol:
+                self.FOLLOW[symbol].add(self.DOLLOR)
+        except:
+            raise Exception('起始符没有设置')
+        return self.FOLLOW[symbol]
+
+def main():
+    ll = LL1Parser()
+    A = Symbol('A','NT')
+    B = Symbol('B','NT')
+    S = Symbol('S','NT')
+    c = Symbol('c','T')
+    a = Symbol('a','T')
+    b = Symbol('b','T')
+   
+    ll.STATEMENTS[S]=[[A,B,c]]
+    ll.STATEMENTS[A]=[[a],[ll.EPSILON]]
+    ll.STATEMENTS[B]=[[b],[ll.EPSILON]]
+    ll.cal_first(S)
+    ll.cal_first(A)
+    ll.cal_first(B)
+    
+    ll.set_start_symbol(S)
+    ll.cal_follow(S)
+    ll.cal_follow(A)
+    ll.cal_follow(B)
+
+    print ll.FIRST[S]
+    print ll.FIRST[A]
+    print ll.FIRST[B]
+    print ll.FOLLOW[S]
+    print ll.FOLLOW[A]
+    print ll.FOLLOW[B]
+if __name__ == '__main__':
+    main()
--- 编译原理/basic/definition.py
+++ 编译原理/basic/definition.py
@@ -43,3 +43,6 @@ class Symbol:
             return True
         else:
             return False
+
+    def __repr__(self):
+        return self.name
